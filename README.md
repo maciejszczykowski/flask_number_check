c6199827f0cc71c15d6dd624dbb7975c25413f8b
Maciej Szczykowski <maciej.szczykowski@gmail.com>
2023-11-09 13:23:29 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+About: Project to train python/flask/html/css/postgresqlDB and CI/CD
+How it works: After entering a number on html frontend, python backend is asking DB if provided number appears there or not and gives back the information to frontend.
+To run project: python app.py
+
+Folders:
+Project name/static/css/style.css
+Project name/templates/index.html
+Project name/app.py
---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+app.static_folder = 'static'
+
+# PostgreSQL database configuration
+db_params = {
+    'database': 'Lottery_DB',
+    'user': 'postgres',
+    'password': 'Norw1ch!po',
+    'host': 'localhost',
+}
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    result = None
+    if request.method == 'POST':
+        number = request.form['number']
+        result = check_number_in_db(number)
+    return render_template('index.html', result=result)
+
+def check_number_in_db(number):
+    conn = psycopg2.connect(**db_params)
+    cursor = conn.cursor()
+    
+    # Query the database to check if the number exists
+    cursor.execute("SELECT * FROM numbers WHERE number = %s", (number,))
+    
+    if cursor.fetchone():
+        result = "Congratulations! You win!"
+    else:
+        result = "Sorry, not this time"
+    
+    conn.close()
+    return result
+
+if __name__ == '__main__':
+    app.run(debug=True)
